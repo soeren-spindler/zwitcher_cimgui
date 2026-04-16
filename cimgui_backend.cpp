5999b4719cb4578c0d4e5e45cda6f940c9898ae4
Sören Spindler <soeren.s89@googlemail.com>
2026-04-16 17:18:34 +0200

    Add win32 backend functions

--- cimgui_backend.cpp
+++ cimgui_backend.cpp
@@ -3,11 +3,31 @@
 #include "imgui.h"
 
 extern "C" {
+/*
+ * Win32
+ */
+
 bool igImGui_ImplWin32_Init(void *hwnd) { return ImGui_ImplWin32_Init(hwnd); }
 
 void igImGui_ImplWin32_Shutdown(void) { ImGui_ImplWin32_Shutdown(); }
 
 void igImGui_ImplWin32_NewFrame(void) { ImGui_ImplWin32_NewFrame(); }
+
+void igImGui_ImplWin32_EnableDpiAwareness() { ImGui_ImplWin32_EnableDpiAwareness(); }
+
+// HMONITOR monitor
+float igImGui_ImplWin32_GetDpiScaleForMonitor(void *monitor) {
+    ImGui_ImplWin32_GetDpiScaleForMonitor(monitor);
+}
+
+// HWND hwnd
+float igImGui_ImplWin32_GetDpiScaleForHwnd(void *hwnd) {
+    return ImGui_ImplWin32_GetDpiScaleForHwnd(hwnd);
+}
+
+/*
+ * DirectX 11
+ */
 
 bool igImGui_ImplDX11_Init(void *device, void *device_context) {
     return ImGui_ImplDX11_Init(static_cast<ID3D11Device *>(device),
--- cimgui_backend.h
+++ cimgui_backend.h
@@ -4,14 +4,26 @@
 extern "C" {
 #endif
 
-bool igImGui_ImplWin32_Init(void* hwnd);
+bool igImGui_ImplWin32_Init(void *hwnd);
+
 void igImGui_ImplWin32_Shutdown(void);
+
 void igImGui_ImplWin32_NewFrame(void);
 
-bool igImGui_ImplDX11_Init(void* device, void* device_context);
+void ImGui_ImplWin32_EnableDpiAwareness();
+
+float ImGui_ImplWin32_GetDpiScaleForHwnd(void *hwnd); // HWND hwnd
+
+float ImGui_ImplWin32_GetDpiScaleForMonitor(void *monitor); // HMONITOR monitor
+
+
+bool igImGui_ImplDX11_Init(void *device, void *device_context);
+
 void igImGui_ImplDX11_Shutdown(void);
+
 void igImGui_ImplDX11_NewFrame(void);
-void igImGui_ImplDX11_RenderDrawData(void* draw_data);
+
+void igImGui_ImplDX11_RenderDrawData(void *draw_data);
 
 #ifdef __cplusplus
 }
